c974d709c9a89961be2433ad1f2e697f2e2cce57
rhy3h <rhy3htw@gmail.com>
2026-02-21 16:40:23 +0800

    fix: missing exports for endpoints and interfaces

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ import { NewsFeedApi } from './endpoints/NewsFeedApi';
 import { IAuthProvider } from './interfaces/IAuthProvider';
 import { IVersionProvider } from './interfaces/IVersionProvider';
 
-export interface RiotApiConfig {
+interface RiotApiConfig {
   authProvider: IAuthProvider;
   versionProvider: IVersionProvider;
 }
@@ -44,4 +44,24 @@ class RiotApi {
   }
 }
 
-export { RiotApi };
+export * from './RiotClient';
+
+export * from './endpoints/AuthApi';
+export * from './endpoints/ContractApi';
+export * from './endpoints/NewsFeedApi';
+export * from './endpoints/PreGameApi';
+export * from './endpoints/PvpApi';
+export * from './endpoints/StoreApi';
+
+export * from './interfaces/DailyTicket';
+export * from './interfaces/IAuthProvider';
+export * from './interfaces/IVersionProvider';
+export * from './interfaces/NewsFeed';
+export * from './interfaces/OwnedItems';
+export * from './interfaces/RiotClientConfig';
+export * from './interfaces/ValorantType';
+
+export * from './utils/authUrl';
+export * from './utils/parseAuthRedirect';
+
+export { RiotApi, RiotApiConfig };
