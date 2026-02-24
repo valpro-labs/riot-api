5deaf37151f428966f4955087737fb1946a638b5
rhy3h <rhy3htw@gmail.com>
2026-02-24 22:53:31 +0800

    feat: update item types

--- src/interfaces/ValorantType.ts
+++ src/interfaces/ValorantType.ts
@@ -14,10 +14,10 @@ export const ITEM_TYPES = {
   AGENT: '01bb38e1-da47-4e6a-9b3d-945fe4655707',
   CONTRACT: 'f85cb6f7-33e5-4dc8-b609-ec7212301948',
   SPARY: 'd5f120f8-ff8c-4aac-92ea-f2b5acbe9475',
-  BUDDY: 'dd3bf334-87f3-40bd-b043-682a57a8dc3a',
+  GUN_BUDDY: 'dd3bf334-87f3-40bd-b043-682a57a8dc3a',
   CARD: '3f296c07-64c3-494c-923b-fe692a4fa1bd',
-  SKIN_LEVEL: 'e7c63390-eda7-46e0-bb7a-a6abdacd2433',
-  // TODO: Skin Variants
+  SKIN: 'e7c63390-eda7-46e0-bb7a-a6abdacd2433',
+  SKIN_VARIANT: '3ad1b2b2-acdb-4524-852f-954a76ddae0a',
   TITLE: 'de7caa6b-adf7-4588-bbd1-143831e786c6',
   FLEX: '03a572de-4234-31ed-d344-ababa488f981',
 };
