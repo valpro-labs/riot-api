8122004227a7008605d578ccbb549a329355b91a
rhy3h <rhy3htw@gmail.com>
2026-02-28 00:23:22 +0800

    refactor: update order

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,52 @@
+// Core
 export * from './RiotApi';
 export * from './RiotClient';
 
+// Endpoints
 export * from './endpoints/AuthApi';
 export * from './endpoints/ContractApi';
 export * from './endpoints/NewsFeedApi';
 export * from './endpoints/PreGameApi';
 export * from './endpoints/PvpApi';
 export * from './endpoints/StoreApi';
-export * from './interfaces/Auth/Entitlement';
-export * from './interfaces/Auth/PlayerInfo';
-export * from './interfaces/Auth/RiotGeo';
-export * from './interfaces/Contract/Contracts';
-export * from './interfaces/Pvp/DailyTicket';
 
+// Base Interfaces & Providers
 export * from './interfaces/Base/IAuthProvider';
 export * from './interfaces/Base/IVersionProvider';
 export * from './interfaces/Base/IXmppAuthProvider';
 export * from './interfaces/Base/IRiotClient';
+export * from './interfaces/Base/RiotClientConfig';
 
+// Auth Interfaces
+export * from './interfaces/Auth/Entitlement';
+export * from './interfaces/Auth/PlayerInfo';
+export * from './interfaces/Auth/RiotGeo';
+
+// Feature Interfaces
+export * from './interfaces/Contract/Contracts';
 export * from './interfaces/General/NewsFeed';
-
-export * from './interfaces/Store/OwnedItems';
-
 export * from './interfaces/PreGame/LockCharacter';
 export * from './interfaces/PreGame/PregameMatch';
 export * from './interfaces/PreGame/PregamePlayer';
 export * from './interfaces/PreGame/SelectCharacter';
+export * from './interfaces/Store/OwnedItems';
+export * from './interfaces/Store/Storefront';
+export * from './interfaces/Store/Wallet';
+
+// PVP Interfaces
 export * from './interfaces/Pvp/AccountXP';
 export * from './interfaces/Pvp/CompetitiveUpdates';
+export * from './interfaces/Pvp/DailyTicket';
 export * from './interfaces/Pvp/MatchDetails';
 export * from './interfaces/Pvp/MatchHistory';
 export * from './interfaces/Pvp/NameService';
 export * from './interfaces/Pvp/PlayerLoadout';
-export * from './interfaces/Base/RiotClientConfig';
 
+// Shared & Common
 export * from './interfaces/Shared/Common';
-export * from './interfaces/Store/Storefront';
-export * from './interfaces/Store/Wallet';
 export * from './interfaces/Shared/ValorantType';
 
+// Sub-modules
 export * from './auth';
-
 export * from './xmpp';
+
