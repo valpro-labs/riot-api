61a3ce233f06d2b297947c8c0bcd5bcc360266c2
rhy3h <rhy3htw@gmail.com>
2026-03-01 17:23:01 +0800

    feat(api): implement 5 new party endpoints

--- src/endpoints/PartyApi.ts
+++ src/endpoints/PartyApi.ts
@@ -21,4 +21,69 @@ export class PartyApi {
       { method: 'GET' }
     );
   }
+
+  /**
+   * [API Docs](https://valapidocs.techchrism.me/endpoint/change-queue)
+   * 
+   * Change the queue for the party
+   */
+  public async changeQueue(region: Region, partyId: string, queueId: string) {
+    return this.client.requestGLZ<PartyResponse>(
+      region,
+      `parties/v1/parties/${partyId}/queue`,
+      { method: 'POST', data: { queueID: queueId } }
+    );
+  }
+
+  /**
+   * [API Docs](https://valapidocs.techchrism.me/endpoint/set-party-accessibility)
+   * 
+   * Set the accessibility of the party
+   */
+  public async setPartyAccessibility(region: Region, partyId: string, accessibility: 'OPEN' | 'CLOSED') {
+    return this.client.requestGLZ<PartyResponse>(
+      region,
+      `parties/v1/parties/${partyId}/accessibility`,
+      { method: 'POST', data: { accessibility: accessibility } }
+    );
+  }
+
+  /**
+   * [API Docs](https://valapidocs.techchrism.me/endpoint/party-set-member-ready)
+   * 
+   * Set the ready status of a player in the current party
+   */
+  public async setMemberReady(region: Region, partyId: string, puuid: string, ready: boolean) {
+    return this.client.requestGLZ<PartyResponse>(
+      region,
+      `parties/v1/parties/${partyId}/members/${puuid}/setReady`,
+      { method: 'POST', data: { ready: ready } }
+    );
+  }
+
+  /**
+   * [API Docs](https://valapidocs.techchrism.me/endpoint/enter-matchmaking-queue)
+   * 
+   * Enter the matchmaking queue for the party
+   */
+  public async enterMatchmakingQueue(region: Region, partyId: string) {
+    return this.client.requestGLZ<PartyResponse>(
+      region,
+      `parties/v1/parties/${partyId}/matchmaking/join`,
+      { method: 'POST' }
+    );
+  }
+
+  /**
+   * [API Docs](https://valapidocs.techchrism.me/endpoint/leave-matchmaking-queue)
+   * 
+   * Leave the matchmaking queue for the party
+   */
+  public async leaveMatchmakingQueue(region: Region, partyId: string) {
+    return this.client.requestGLZ<PartyResponse>(
+      region,
+      `parties/v1/parties/${partyId}/matchmaking/leave`,
+      { method: 'POST' }
+    );
+  }
 }
